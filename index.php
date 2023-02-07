745a8084bb1369d5e73448dc93658918efd4d0ad
iammarwan104 <marwahpinrang6@gmail.com>
2023-02-07 08:48:56 +0700

    menambahkan <p>1 yang baru

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
     <h1>Goog Morning Marwan</h1>
     <p>bagaimana kabar anda</p>
     <p>Saya harap ada baik baik saja dan sedang melakukan sesuatu yang produktif dan bermanfaat</p>
+    <p>jika anda sedang berlajar skill baru maka anda harus bersyukur karena anda sedang melakukan aktifitas yang tepat</p>
 </body>
 
 </html>
